e602ad3458a3b78fef0020dd6e9902cde00bf5c2
RiRiSharp <github@ririsharp.nl>
2025-06-20 23:50:11 +0200

    Add ColorInput component

--- RiRiSharp.Bootstrap/Forms/BsInputBase.cs
+++ RiRiSharp.Bootstrap/Forms/BsInputBase.cs
@@ -6,9 +6,21 @@ namespace RiRiSharp.Bootstrap.Forms;
 
 public abstract class BsInputBase<TValue> : InputBase<TValue>, IBsComponent, IBsFormControlComponent
 {
+    private readonly string _additionalClasses;
+
+    protected BsInputBase(string additionalClasses = "")
+    {
+        if (string.IsNullOrWhiteSpace(additionalClasses))
+        {
+            _additionalClasses = null;
+        }
+
+        _additionalClasses = additionalClasses;
+    }
+
     private const string _formControl = "form-control";
     private const string _formControlPlaintext = "form-control-plaintext";
-    
+
     [Parameter] public string Classes { get; set; }
     [Parameter] public FormSize FormSize { get; set; } = FormSize.Regular;
     [Parameter] public bool ReadonlyPlaintext { get; set; }
@@ -16,6 +28,11 @@ public abstract class BsInputBase<TValue> : InputBase<TValue>, IBsComponent, IBs
     protected override void OnParametersSet()
     {
         base.OnParametersSet();
+        SetClass();
+    }
+
+    private void SetClass()
+    {
         var classes = DetermineClasses();
         AdditionalAttributes = BsAttributeUtilities.AddClasses(AdditionalAttributes, classes);
     }
@@ -24,7 +41,7 @@ public abstract class BsInputBase<TValue> : InputBase<TValue>, IBsComponent, IBs
     {
         var sizeClass = FormSize.ToBootstrapClass();
         var formControl = DeterminePlainTextClass();
-        return string.Join(' ', formControl, sizeClass, Classes);
+        return string.Join(' ', formControl, _additionalClasses, sizeClass, Classes);
     }
 
     private string DeterminePlainTextClass()
---
+++ RiRiSharp.Bootstrap/Forms/BsInputColor.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Drawing;
+
+namespace RiRiSharp.Bootstrap.Forms;
+
+// Solves https://stackoverflow.com/questions/64052566/is-there-any-blazor-timepicker-and-colorpicker
+public partial class BsInputColor() : BsInputBase<Color>("form-control-color")
+{
+    protected override bool TryParseValueFromString(string value, out Color result, out string validationErrorMessage)
+    {
+        result = ColorTranslator.FromHtml(value);
+        validationErrorMessage = null;
+        return true;
+    }
+
+    protected override string FormatValueAsString(Color value)
+    {
+        var htmlColor = ColorTranslator.ToHtml(value);
+        return htmlColor;
+    }
+
+    private void Change(ChangeEventArgs obj)
+    {
+        var test = obj.Value?.ToString();
+        Console.WriteLine(test);
+    }
+}
