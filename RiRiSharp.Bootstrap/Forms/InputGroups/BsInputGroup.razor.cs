6ff3dc1639730e4b6e9a35ff995f0ad07fddde56
RiRiSharp <github@ririsharp.nl>
2025-07-18 15:27:18 +0200

    Add input groups

---
+++ RiRiSharp.Bootstrap/Forms/InputGroups/BsInputGroup.razor.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace RiRiSharp.Bootstrap.Forms.InputGroups;
+
+public partial class BsInputGroup : BsChildContentComponent
+{
+    [Parameter] public InputGroupSize Size { get; set; } = InputGroupSize.Regular;
+}
