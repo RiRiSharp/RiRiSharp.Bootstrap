6c8c16fe9c38b321d3b037210a27db22960c3d3d
RiRiSharp <github@ririsharp.nl>
2025-08-25 22:54:55 +0200

    Add range input tests

---
+++ RiRiSharp.Bootstrap.UnitTests/Forms/Range/BsFormRangeInputTests.cs
@@ -0,0 +1,5 @@
+﻿using RiRiSharp.Bootstrap.Forms.Range;
+
+namespace RiRiSharp.Bootstrap.UnitTests.Forms.Range;
+
+public class BsFormRangeInputTests() : BsInputBaseComponentTests<BsFormRangeInput<int>, int>("""<input class="form-range {0}" type="range" value="0" {1}>""");
