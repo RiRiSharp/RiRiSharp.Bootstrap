0e36c0fa0928f48dccea5c1b25601bf7b94d295e
RiRiSharp <github@ririsharp.nl>
2025-09-22 16:56:10 +0200

    Add toggle buttons

---
+++ src/RiRiSharp.Bootstrap/Components/Buttons/BsToggleButton.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Components;
+using RiRiSharp.Bootstrap.Components.Buttons.Internals;
+
+namespace RiRiSharp.Bootstrap.Components.Buttons;
+
+public class BsToggleButton : BsButton, IAsyncDisposable
+{
+    [Inject]
+    private IBsButtonJsFunctions ButtonJsFunctions { get; set; } = null!;
+
+    public async Task ToggleAsync()
+    {
+        await ButtonJsFunctions.ToggleAsync(HtmlRef);
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await DisposeAsync(true);
+        GC.SuppressFinalize(this);
+    }
+
+    private async ValueTask DisposeAsync(bool disposing)
+    {
+        if (!disposing)
+        {
+            return;
+        }
+
+        await ButtonJsFunctions.DisposeAsync(HtmlRef);
+    }
+}
