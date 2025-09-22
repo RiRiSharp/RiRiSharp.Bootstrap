aaedc551c16e46a42d205dbdcb009b177b59fc6d
RiRiSharp <github@ririsharp.nl>
2025-09-22 13:48:36 +0200

    Breadcrumb and badge parameter tests

--- src/RiRiSharp.Bootstrap.UnitTests/Components/Breadcrumb/BsBreadcrumbTests.cs
+++ src/RiRiSharp.Bootstrap.UnitTests/Components/Breadcrumb/BsBreadcrumbTests.cs
@@ -2,4 +2,4 @@ using RiRiSharp.Bootstrap.Components.Breadcrumb;
 
 namespace RiRiSharp.Bootstrap.UnitTests.Components.Breadcrumb;
 
-public class BsBreadcrumbTests() : BsComponentTests<BsBreadcrumb>("""<ol class="breadcrumb {0}" {1}></ol>""");
+public class BsBreadcrumbTests() : BsComponentTests<BsBreadcrumb>("""<ol class="breadcrumb {0}" {1}></ol>""") { }
