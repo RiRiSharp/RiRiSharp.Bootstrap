2dfb5beb7cd0d3e58edf4805c10a49859a5b4efc
RiRiSharp <github@ririsharp.nl>
2026-01-17 18:10:24 +0100

    Add forgotten option to nav bar

--- src/RiRiSharp.Bootstrap/Components/NavBar/BsNavbarNav.razor.cs
+++ src/RiRiSharp.Bootstrap/Components/NavBar/BsNavbarNav.razor.cs
@@ -4,5 +4,7 @@ namespace RiRiSharp.Bootstrap.Components.NavBar;
 
 public partial class BsNavbarNav : BsChildContentComponent
 {
-    protected override string BsComponentClasses => "navbar-nav";
+    protected override string BsComponentClasses => $"navbar-nav {ModeClass}";
+    public NavbarScrollingMode Mode { get; set; }
+    private string ModeClass => Mode.ToBootstrapClass();
 }
---
+++ src/RiRiSharp.Bootstrap/Components/NavBar/NavbarScrollingMode.cs
@@ -0,0 +1,20 @@
+namespace RiRiSharp.Bootstrap.Components.NavBar;
+
+public enum NavbarScrollingMode
+{
+    Regular = 0,
+    EnableVerticalScrolling = 1,
+}
+
+public static class NavbarScrollingModeExtensions
+{
+    public static string ToBootstrapClass(this NavbarScrollingMode mode)
+    {
+        return mode switch
+        {
+            NavbarScrollingMode.Regular => "",
+            NavbarScrollingMode.EnableVerticalScrolling => "navbar-nav-scroll",
+            _ => throw new ArgumentOutOfRangeException(nameof(mode), mode, null),
+        };
+    }
+}
